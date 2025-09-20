e4de6f2310e5c0444dc9c5da8a9d34ce8fbfe48f
LuBashQ <cristian.lupascu.nicolae@gmail.com>
2025-09-20 21:25:24 +0300

    add theme catppuccin-mocha
    Signed-off-by: LuBashQ <cristian.lupascu.nicolae@gmail.com>

--- config.def.h
+++ config.def.h
@@ -10,14 +10,21 @@ static const char *fonts[] = {
 	"LiterationMonoNerdFont:size=16"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static char normfgcolor[]                = "#cdd6f4";
+static char normbgcolor[]                = "#1e1e2e";
+
+static char selfgcolor[]                 = "#ffffff";
+static char selbgcolor[]                 = "#89b4fa";
+
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { normfgcolor, normbgcolor },
+	[SchemeSel] = { selfgcolor, selbgcolor },
+	[SchemeOut] = { selfgcolor, selbgcolor},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 1;
+static unsigned int lines      = 10;
 /* -h option; minimum height of a menu line */
 static unsigned int lineheight = 10;
 static unsigned int min_lineheight = 8;
